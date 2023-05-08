a749a1cc29297dc0c75dcf7002b7da0337720341
DKNS-Usal <jorgecuadradojcc@usal.es>
2023-05-08 20:36:35 +0200

    🔥 nuevo manejo cadenas

--- manejo_cadenas.asm
+++ manejo_cadenas.asm
@@ -7,10 +7,13 @@ pantalla 	.equ 	0xFF00
                 .globl imprime_cadena
                 .globl limpia_pantalla
                 .globl error_switch
+                .globl imprime_cadena_seguida
+                .globl salir
 
 limpia_pantalla:  .asciz  "\033[2J"
-salir:            .asciz  "\nPulse la tecla c para salir.\n"
+salir:            .asciz  "\n\n\nPulse la tecla c para salir.\n"
 error_switch:     .asciz  "\33[31mOpcion incorrecta, intentelo de nuevo.\n"
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; imprime_cadena                                                   ;
@@ -31,6 +34,25 @@ sgte:
 ret_imprime_cadena:
         puls    a               ; Recupera el valor de A
         rts                     ; y sale de la subrutina equivalente a puls pc (return)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+imprime_cadena_seguida:
+    pshs a            ; Guarda el valor de A en la pila
+    clrb        ; Inicializa el contador a 0
+numero_sgte:
+    lda ,x+     ; Carga el primer caracter de la cadena
+    beq loop    ; Si es 0, sale a ret_imprime_cadena
+    cmpa #'\n   ; Compara el caracter con el salto de linea
+    beq contador        ; Si es igual, salta a contador
+    sta pantalla        ; Si no, lo imprime en pantalla
+    bra numero_sgte        ; y vuelve a sgte
+contador:
+    incb        ; Incrementa el contador
+    bra loop    ; Vuelve a numero_sgte
+loop:
+    puls a      
+    rts
 
 
 
