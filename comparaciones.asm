377f35f1cf126ba9cd06ef5899672a07b5cd520c
DKNS-Usal <jorgecuadradojcc@usal.es>
2023-05-10 19:17:30 +0200

    ✨ Comparaciones NEW

--- comparaciones.asm
+++ comparaciones.asm
@@ -1,4 +1,4 @@
-       .module comparaciones
+       .module comparaciones_prueba
        pantalla     .equ   0xFF00
        teclado      .equ   0xFF02
                     
@@ -6,18 +6,25 @@
 primer_5cifras: .asciz "Primer premio de 5 cifras equivale a 1000 puntos\n"
 segundo_5cifras:.asciz "Segundo premio de 5 cifras equivale a 500 puntos\n"
 tercero_5cifras:.asciz "Tercer premio de 5 cifras equivale a 200 puntos\n"
-cuatro_cifras:  .asciz "Terminacion de 4 cifras equivale a 50 puntos\n"
-tres_cifras:    .asciz "Terminacion de 3 cifras equivale a 10 puntos\n"
-dos_cifras:     .asciz "Terminacion de 2 cifras equivale a 5 puntos\n"
-una_cifra:     .asciz "Terminacion de 1 cifra (reintegro) equivale a 1 punto\n"
+cifras4:        .asciz "Terminacion de 4 cifras equivale a 50 puntos\n"
+cifras3:        .asciz "Terminacion de 3 cifras equivale a 10 puntos\n"
+cifras2:        .asciz "Terminacion de 2 cifras equivale a 5 puntos\n"
+cifra1:         .asciz "Terminacion de 1 cifra (reintegro) equivale a 1 punto\n"
+
+resultado_txt:  .ascii "El resultado es: "
 
 ;Declaraciones
-puntuaje:       .word 0
-numeros.cuatrocifras: .byte 0
-numeros.trescifras: .byte 0
-numeros.doscifras: .byte  0
-numeros.reintegro: .byte 0
+numeros_sorteo: .byte   0
+puntuaje:       .word   0
+puntuaje_total: .word   0
 
+numeros.cuatrocifras:   .byte   2
+numeros.trescifras:     .byte   14
+numeros.doscifras:      .byte   5
+numeros.reintegro:      .byte   3
+
+                .globl programa
+                .globl  valor_decimos
                 .globl  primer.premio
                 .globl  segundo.premio
                 .globl  tercer.premio
@@ -25,248 +32,174 @@ numeros.reintegro: .byte 0
                 .globl  tres.cifras
                 .globl  dos.cifras
                 .globl  reintegro
-
                 .globl  imprime_cadena
-
-                .globl  comparacion_primero
-                .globl  comparacion_segundo
-                .globl  comparacion_tercero
-                .globl  comparacion_4cifras
-                .globl  comparacion_3cifras
-                .globl  comparacion_2cifras
-                .globl  comparacion_reintegros
-
-                .globl  programa
-                .globl  valor_decimos
-                
-
-;Itinerancia de los numeros del sorteo
+                .globl  programa_comparaciones
 
 
-;Comparacion del primer premio
-comparacion_primero:
-                ldx     #valor_decimos
-                ldy     #primer.premio
-compara_bucle1:
-		lda     ,x+
-		beq     compara_igual1
-		cmpa    ,y+
-		beq     compara_bucle1
-compara_distinto1:
-                ldd     #0
-		jsr    comparacion_segundo
-compara_igual1:
-		ldd     puntuaje
-                addd    #1000
-                std     puntuaje
-                jsr     comparacion_4cifras
-                
-compara_fin1:
-		addd    #'0
-		stb     pantalla			
-                ldb     #'\n
-		stb     pantalla
-                jsr     comparacion_4cifras
+programa_comparaciones:
+        ;rotacion de numeros
+
+        comparacion_primero:
+        ldx     #valor_decimos
+        ldy     #primer.premio
+        jsr     compara_bucle
+        cmpb    #0
+        bne     else1
+        ldd     #0
+        bra     comparacion_segundo
+        else1:
+        ldd     #1000
+        std     puntuaje
+        ldd     #primer_5cifras
+        jsr     imprime_cadena
+        ldd     #0
+        bra     comparacion_4cifras
 
 
-
-;Comparacion del segundo premio
-comparacion_segundo:
-                ldx     #valor_decimos
-                ldy     #segundo.premio
-compara_bucle2:
-		lda     ,x+
-		beq     compara_igual2
-		cmpa    ,y+
-		beq     compara_bucle2
-compara_distinto2:
-                ldd     #0
-		jsr     comparacion_tercero
-compara_igual2:
-		ldd     puntuaje
-                addd    #500
-                std     puntuaje
-                jsr     comparacion_4cifras
-                
-compara_fin2:
-		addd    #'0
-		stb     pantalla			
-                ldb     #'\n
-		stb     pantalla
-                jsr     comparacion_4cifras
+        comparacion_segundo:
+        ldx     #valor_decimos
+        ldy     #segundo.premio
+        jsr     compara_bucle
+        cmpb    #0
+        bne     else2
+        ldd     #0
+        bra     comparacion_tercero
+        else2:
+        ldd     puntuaje
+        addd    #500
+        ldd     #segundo_5cifras
+        jsr     imprime_cadena
+        ldd     #0
+        bra     comparacion_4cifras
 
 
-
-;Comparacion del tercer premio
-comparacion_tercero:
-                ldx     #valor_decimos
-                ldy     #tercer.premio
-compara_bucle3:
-		lda     ,x+
-		beq     compara_igual3
-		cmpa    ,y+
-		beq     compara_bucle3
-compara_distinto3:
-                ldd     #0
-		jsr     comparacion_4cifras
-compara_igual3:
-		ldd     puntuaje
-                addd    #200
-                std     puntuaje
-                jsr     comparacion_4cifras
-                
-compara_fin3:
-		addd    #'0
-		stb     pantalla			
-                ldb     #'\n
-		stb     pantalla
-                jsr     comparacion_4cifras
+        comparacion_tercero:
+        ldx     #valor_decimos
+        ldy     #tercer.premio
+        jsr     compara_bucle
+        cmpb    #0
+        bne     else3
+        ldd     #0
+        bra     comparacion_4cifras
+        else3:
+        ldd     puntuaje
+        addd    #200
+        ldd     #tercero_5cifras
+        jsr     imprime_cadena
+        ldd     #0
+        bra     comparacion_4cifras
 
 
-;Comparacion de numeros de 4 cifras
-comparacion_4cifras:
-                ldx     #valor_decimos
-                leax    1, x  
-                ldy     #tres.cifras
-
-                pshs    a
-                lda     #2
-                sta     numeros.trescifras
-                puls    a
-compara_bucle4:
-		lda     ,x+
-		beq     compara_igual4
-		cmpa    ,y+
-		beq     compara_bucle4
-
-                dec     numeros.trescifras
-                bne     compara_bucle4
-compara_distinto4:
-                ldd     #0
-		jsr     comparacion_3cifras
-compara_igual4:
-		ldd     puntuaje
-                addd    #50
-                std     puntuaje
-                jsr     comparacion_3cifras
-                
-compara_fin4:
-		addd    #'0
-		stb     pantalla			
-                ldb     #'\n
-		stb     pantalla
-                jsr     comparacion_3cifras
+        comparacion_4cifras:
+        lda     numeros.cuatrocifras
+        ldx     #valor_decimos
+        cmpa    #0
+        beq     comparacion_3cifras
+        dec     numeros.cuatrocifras
+        bucle_numeros4cifras:
+        leax    1,x
+        ldy     #cuatro.cifras
+        cmpb    #0
+        bne     else4
+        bra     comparacion_4cifras
+        else4:
+        clra
+        clrb
+        ldd     puntuaje
+        addd    #50
+        ldd     #cifras4
+        jsr     imprime_cadena
+        jsr     comparacion_3cifras
 
 
-
-;Comparacion de numeros de 3 cifras
-comparacion_3cifras:
-                ldx     #valor_decimos
-                leax    1, x  
-                ldy     #tres.cifras
-
-                pshs    a
-                lda     #14
-                sta     numeros.trescifras
-                puls    a
-compara_bucle5:
-		lda     ,x+
-		beq     compara_igual5
-		cmpa    ,y+
-		beq     compara_bucle4
-
-                dec     numeros.doscifras
-                bne     compara_bucle5
-compara_distinto5:
-                ldd     #0
-		jsr     comparacion_2cifras
-compara_igual5:
-		ldd     puntuaje
-                addd    #10
-                std     puntuaje
-                jsr     comparacion_2cifras
-                
-compara_fin5:
-		addd    #'0
-		stb     pantalla			
-                ldb     #'\n
-		stb     pantalla
-                jsr     comparacion_2cifras
+        comparacion_3cifras:
+        lda     numeros.trescifras
+        ldx     #valor_decimos
+        cmpa    #0
+        beq     comparacion_2cifras
+        dec     numeros.cuatrocifras
+        bucle_numeros3cifras:
+        leax    2,x
+        ldy     #cuatro.cifras
+        cmpb    #0
+        bne     else5
+        bra     comparacion_3cifras
+        else5:
+        clra
+        clrb
+        ldd     puntuaje
+        addd    #5
+        ldd     #cifras3
+        jsr     imprime_cadena
+        jsr     comparacion_2cifras
 
 
-
-;Comparacion de terminaciones de 2 cifras
-comparacion_2cifras:
-                ldx     #valor_decimos
-                leax    1, x  
-                ldy     #cuatro.cifras
-
-                pshs    a
-                lda     #5
-                sta     numeros.doscifras
-                puls    a
-compara_bucle6:
-		lda     ,x+
-		beq     compara_igual6
-		cmpa    ,y+
-		beq     compara_bucle6
-
-                dec     numeros.doscifras
-                bne     compara_bucle6
-compara_distinto6:
-                ldd     #0
-		jsr     comparacion_reintegros
-compara_igual6:
-		ldd     puntuaje
-                addd    #5
-                std     puntuaje
-                jsr     comparacion_reintegros
-                
-compara_fin6:
-		addd    #'0
-		stb     pantalla			
-                ldb     #'\n
-		stb     pantalla
-                jsr     comparacion_reintegros
+        comparacion_2cifras:
+        lda     numeros.doscifras
+        ldx     #valor_decimos
+        cmpa    #0
+        beq     comparacion_3cifras
+        dec     numeros.doscifras
+        bucle_numeros2cifras:
+        leax    3,x
+        ldy     #dos.cifras
+        cmpb    #0
+        bne     else6
+        bra     comparacion_4cifras
+        else6:
+        clra
+        clrb
+        ldd     puntuaje
+        addd    #5
+        ldd     #cifras2
+        jsr     imprime_cadena
+        jsr     comparacion_reintegros
 
 
+        comparacion_reintegros:
+        lda     numeros.reintegro
+        ldx     #valor_decimos
+        cmpa    #0
+        beq     salto
+        dec     numeros.reintegro
+        bra     bucle_numeros4cifras
+        salto:
+        jsr     programa_comparaciones
+        bucle_numeros1cifras:
+        leax    4,x
+        ldy     #reintegro
+        cmpb    #0
+        bne     else7
+        bra     comparacion_reintegros
+        else7:
+        clra
+        clrb
+        ldd     puntuaje
+        addd    #1
+        ldd     #cifra1
+        jsr     imprime_cadena
+        jsr     programa_comparaciones
 
-;Comparaciones de reintegros
-comparacion_reintegros:
-                ldx     #valor_decimos
-                leax    1, x  
-                ldy     #reintegro
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;comparacion de 2 numeros                                                                                              ;
+; compara lso numeros de los registros X e Y y da un valor de retorno                                                  ;
+;                                                                                                                      ;       
+; Entrada: X-direcciOn de comienzo de la cadena                                                                        ;
+; Salida: b con un valor de retorno                                                                                    ;   
+; Registros afectados: X, A, B, Y.                                                                                     ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+compara_bucle:
+	lda     ,x+
+	beq     compara_IGUAL
+	cmpa    ,y+
+	beq     compara_bucle
+compara_DISTINTO:
+	ldb     #0
+	bra     compara_fin
 
-                pshs    a
-                lda     #3
-                sta     numeros.reintegro
-                puls    a
-compara_bucle7:
-		lda     ,x+
-		beq     compara_igual7
-		cmpa    ,y+
-		beq     compara_bucle7
-
-                dec     cuatro.cifras
-                bne     compara_bucle7
-compara_distinto7:
-                ldd     #0
-		jsr     fin_comparacion
-compara_igual7:
-		ldd     puntuaje
-                addd    #1
-                std     puntuaje
-                jsr     fin_comparacion     
-                
-compara_fin7:
-		addd    #'0
-		stb     pantalla			
-                ldb     #'\n
-		stb     pantalla
-                jsr     fin_comparacion
-
-fin_comparacion:
-                ldd    puntuaje
-                std    pantalla
-                jsr    programa
-
+compara_IGUAL:
+	ldb    #1
+        bra    compara_fin
+compara_fin:
+        rts
+		
