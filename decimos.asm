e88382adbbfa5d018dfc0052ea17e16fe38dc7fe
DKNS-Usal <jorgecuadradojcc@usal.es>
2023-05-10 16:25:30 +0200

    add for_introducir

--- decimos.asm
+++ decimos.asm
@@ -34,8 +34,12 @@ valor_decimos:  .asciz  "89603"
 
 decimos_MAX: .byte 11
 decimos_NUM: .byte 11
+contador:    .byte 0
+temp:        .byte 0
+tam_decimo:  .byte 0
 
 ver:     .asciz "Los decimos actuales son...\n\n"
+txtintroducirdecimos: .asciz "\nCuantos decimos va a introducir?:"
 
 
 decimos:
@@ -49,7 +53,7 @@ decimos:
     cmpa #'1 ; 1. Ver
     beq ver_decimos; Si es 1, va a decimos_ver
     cmpa #'2 ; 2. Introducir resultados
-    beq decimos; CAMBIAR
+    beq decimos_introducir; CAMBIAR
     cmpa #'3 ; 3. Volver
     beq programa_decimos    ; Si es 3, vuelve al menu principal
     ldx #error_switch
@@ -89,5 +93,55 @@ salir_bucle0:
     ldx #error_switch
     jsr imprime_cadena
     bra ver_decimos
+
+decimos_introducir:
+    ldx #limpia_pantalla
+    jsr imprime_cadena
+    ldx #txtintroducirdecimos
+    jsr imprime_cadena
+    lda teclado
+    sta decimos_NUM
+    ldx #valor_decimos
+    stx temp
+
+for_introducir:
+
+        lda decimos_NUM
+        sta contador
+
+bucle_decimos:
+
+        lda #5
+        sta tam_decimo
+
+cargadecimo:
+
+        lda teclado
+        sta ,x+
+        dec tam_decimo
+        bne cargadecimo
+
+        ldx temp
+        leax 6, x
+        stx temp
+
+        ldb #'\n
+        stb pantalla
+
+        dec contador
+        bne bucle_decimos
+
+fin_introducir:
+
+        ldx #salir
+        jsr imprime_cadena
+        lda teclado
+        cmpa #'c
+        lbeq decimos
+        cmpa #'C
+        lbeq decimos
+        ldx #error_switch
+        jsr imprime_cadena
+        bra fin_introducir
 
 
