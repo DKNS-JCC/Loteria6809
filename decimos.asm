a44e902c802c4e34d5d956e9cda4c096d4e04d5c
DKNS-Usal <jorgecuadradojcc@usal.es>
2023-05-11 21:06:21 +0200

    💥 FIN DECIMOS

--- decimos.asm
+++ decimos.asm
@@ -14,8 +14,11 @@ pantalla 	.equ 	0xFF00
 
                 .globl valor_decimos
                 .globl decimos
-var1: .byte 0
-var2: .byte 0
+                .globl imprime_num
+                .globl acumulador
+
+cuenteo: .byte 0
+
 
 m_decimos:      .ascii  "\n\33[32m=========DECIMOS==========\n"
                 .ascii  "\33[33m1. Ver\n"
@@ -39,6 +42,10 @@ decimos_NUM: .byte 11
 contador:    .byte 0
 temp:        .byte 0
 tam_decimo:  .byte 0
+n1:          .byte 0
+n2:          .byte 0
+n_digitos:   .byte 0
+
 
 ver:     .asciz "Los decimos actuales son...\n\n"
 txtintroducirdecimos: .asciz "\nCuantos decimos va a introducir?:"
@@ -68,24 +75,14 @@ programa_decimos:
 ver_decimos:
     ldx #limpia_pantalla
     jsr imprime_cadena
-    ;LIMPIA PANTALLA
     ldx #ver
     jsr imprime_cadena
-    
-    lda #0
-    ldx #valor_decimos
-    
-for:
+	lda #'\n
+	sta pantalla
 
-    cmpa decimos_NUM
-    bge salir_bucle0
-    jsr imprime_cadena
-    adda #1
-    ldb #'\n
-    stb pantalla
-    bra for
-
-salir_bucle0:
+	ldb decimos_NUM
+	ldx #valor_decimos
+	jsr imprime_num
 
     ldx #salir
     jsr imprime_cadena
@@ -106,57 +103,52 @@ decimos_introducir:
 
     lda teclado
     suba #'0
-    sta var1
+    sta n1
     lda teclado
     suba #'0
-    sta var2
+    sta n2
 
     ldb #10
-    lda var1
+    lda n1
     mul
-    addb var2
+    addb n2
+    
+    cmpb #decimos_MAX
+    bhi decimos_introducir
+
+
+    lda #'\n
+    sta pantalla
 
     stb decimos_NUM
     
     ldx #valor_decimos
-    stx temp
+    stx temp ; Guardamos la direccion de memoria de valor_decimos en temp para modificarlo sin afectar a valor_decimos
 
 for_introducir:
 
+        clr acumulador
         lda decimos_NUM
-        sta contador
+        sta acumulador
 
-bucle_decimos:
+decimo_a_decimo:
 
-        lda #5
-        sta tam_decimo
+        lda #5 ; 5 numeros por decimo
+        sta cuenteo   ; n_digitos = 5
 
-cargadecimo:
+digitos:
 
-        lda teclado
-        sta ,x+
-        dec tam_decimo
-        bne cargadecimo
-
-        ldx temp
-        leax 6, x
-        stx temp
-
-        ldb #'\n
-        stb pantalla
-
-        dec contador
-        bne bucle_decimos
+        lda teclado ; Cargamos el numero introducido por teclado
+        sta ,x+    ; Lo guardamos en la direccion de memoria de temp        
+        dec cuenteo ; Decrementamos el contador
+        bne digitos ; Si el contador es distinto de 0, repetimos el bucle
+        ldx temp    ; Cargamos la direccion de memoria de temp en x
+        leax 6, x   ; Le sumamos 6 a x para que apunte al siguiente decimo
+        stx temp    ; Guardamos la direccion de memoria de temp en temp
+        ldb #'\n        ; Cargamos el salto de linea en b
+        stb pantalla    ; Guardamos el salto de linea en pantalla
+        dec acumulador      ; Decrementamos el acumulador
+        bne decimo_a_decimo   ; Si el acumulador es distinto de 0, repetimos el bucle
 
 fin_introducir:
-
-        ldx #salir
-        jsr imprime_cadena
-        lda teclado
-        cmpa #'c
-        lbeq decimos
-        cmpa #'C
-        lbeq decimos
-        ldx #error_switch
-        jsr imprime_cadena
-        bra fin_introducir
+        jsr decimos ; Volvemos a decimos
